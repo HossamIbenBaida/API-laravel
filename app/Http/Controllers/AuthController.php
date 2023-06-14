405022c3030d73ddc5a74aa60e747ccec001aa4b
Sword <hossamsword97@gmail.com>
2023-06-14 23:40:09 +0100

    finishing register methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -9,13 +10,13 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
-    public function register(Request $request){
+    public function register(RegisterRequest $request){
         $user = User::create([
             'first_name' => $request->input('first_name'),
             'last_name' => $request->input('last_name'),
             'email' => $request->input('email'),
             'password' => Hash::make($request->input('password')),
-
+            //'password_confirm' => Hash::make($request->input('password_confirm')),
             
         ]);
         return response($user , Response::HTTP_CREATED);
