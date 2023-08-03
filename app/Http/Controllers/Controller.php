fe82601ac1218e049462f5ac387ddc29f8362f8d
Sword <hossamsword97@gmail.com>
2023-08-03 23:18:57 +0100

    swagger documentation

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,30 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Admin Documentation",
+ *      description="Admin open API description",
+ *      @OA\Contact(
+ *          email="hossamibenbaida@gmail.com"
+ *      ),
+ *      @OA\Server(
+ *          url=L5_SWAGGER_CONST_HOST ,
+ *         description="Admin API Server",
+ *      )
+ * )
+ * 
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     securityScheme="bearerAuth",
+ *     scheme="bearer",
+ *     description="Bearer token authentication"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,14 +12,43 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-  
+ 
+ /**
+ * @OA\Get(
+ *     path="/api/users",
+ *     security={{"bearerAuth":{}}},
+ *     summary="Get list of users",
+ *     @OA\Parameter(
+ *         name="page",
+ *         description="Pagination page",
+ *         in="query",
+ *         @OA\Schema(
+ *             type="integer"
+ *         )
+ *     ),
+ *     @OA\Response(response="200", description="User Collection", @OA\JsonContent())
+ * )
+ */
+
+
     public function index()
     {
         $this->authorize('view', 'users');
        return UserResource::collection(User::with('role')->paginate(15));
     }
-
-  
+/**
+ * @OA\Post(
+ *     path="/api/users/",
+ *     security={{"bearerAuth":{}}},
+ *     @OA\Response(response="201" ,
+ *        description="User",
+ *         ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(ref="#/components/schemas/UserCreateRequest")
+ *     )
+ * )
+ */
     public function store(UserCreateRequest $request)
     {
         $this->authorize('edit', 'users');
@@ -29,14 +58,50 @@ class UserController extends Controller
        return response(new UserResource($user) , Response::HTTP_CREATED );
     }
 
- 
+ /**
+ * @OA\Get(
+ *     path="/api/users/{id}",
+ *     security={{"bearerAuth":{}}},
+ *     summary="Get list of users",
+ *     @OA\Parameter(
+ *         name="id",
+ *         description="user id",
+ *         in="path",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="integer"
+ *         )
+ *     ),
+ *     @OA\Response(response="200", description="User Collection", @OA\JsonContent())
+ * )
+ */
     public function show($id)
     {
         $this->authorize('view', 'users');
         return new UserResource(User::with('role')->find($id));
     }
 
- 
+ /**
+ * @OA\Put(
+ *     path="/api/users/{id}",
+ *     security={{"bearerAuth":{}}},
+ *     @OA\Response(response="202" ,
+ *        description="User"),
+ *     @OA\Parameter(
+ *         name="id",
+ *         description="user id",
+ *         in="path",
+ *         @OA\Schema(
+ *             type="integer"
+ *         )
+ *     ),
+ *     @OA\RequestBody(
+ *         required=true,
+ *         @OA\JsonContent(ref="#/components/schemas/UserUpdateRequest")
+ *     )
+ * )
+ */
+
     public function update(UserUpdateRequest $request, $id)
     {
         $this->authorize('edit', 'users');
@@ -45,7 +110,23 @@ class UserController extends Controller
         return response(new UserResource($user) , Response::HTTP_ACCEPTED);
     }
 
- 
+ /**
+ * @OA\Delete(
+ *     path="/api/users/{id}",
+ *     security={{"bearerAuth":{}}},
+ *     summary="Delete of user",
+ *     @OA\Parameter(
+ *         name="id",
+ *         description="user id",
+ *         in="path",
+ *         required=true,
+ *         @OA\Schema(
+ *             type="integer"
+ *         )
+ *     ),
+ *     @OA\Response(response="204", description="User Collection", @OA\JsonContent())
+ * )
+ */
     public function destroy($id)
     {
        $this->authorize('edit', 'users');
